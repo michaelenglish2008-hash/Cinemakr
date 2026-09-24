5dcc03f494c869917d8d4fd3fda8f97a9c009153
michaelenglish2008-hash <michaelenglish2008@gmail.com>
2026-09-24 17:47:51 -0300

    Create MainActivity.java

---
+++ app/src/main/java/com/cinemakr/MainActivity.java
@@ -0,0 +1,29 @@
+package com.cinemakr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/www/index.html");
+
+        setContentView(webView);
+    }
+}
